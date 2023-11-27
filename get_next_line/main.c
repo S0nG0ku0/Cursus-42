3fe4f4c32d8b7f561663795d8407ae47af8dcf55
Omar Haida <ohaida@e3r7p8.1337.ma>
2023-11-27 12:15:37 +0100

    libft

---
+++ get_next_line/main.c
@@ -0,0 +1,27 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int     BUFFER_SIZE = 30;
+
+int     main(void)
+{
+        int             fd;
+        ssize_t bytes_read;
+        char    b[BUFFER_SIZE + 1];
+        int             i;
+
+        fd = open("./text.txt", O_RDONLY);
+        i = 0;
+        while ((bytes_read = read(fd, b, BUFFER_SIZE)) > 0)
+        {
+                for (int j = 0; j < bytes_read; j++)
+                {
+                        if (b[j] != '\n')
+                        {
+                                write(1, &b[j], 1);
+                        }
+                }
+                close(fd);
+        }
+}
--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,16 +6,31 @@
 /*   By: ohaida <ohaida@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 21:58:16 by ohaida            #+#    #+#             */
-/*   Updated: 2023/11/02 22:25:28 by ohaida           ###   ########.fr       */
+/*   Updated: 2023/11/24 15:21:38 by ohaida           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	isoverflow(long long a, long long b)
+{
+	long long	result;
+
+	if (a == 0 || b == 0)
+		return (0);
+	result = a * b;
+	if (a == result / b)
+		return (0);
+	else
+		return (1);
+}
+
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*alloc;
 
+	if (isoverflow(count, size))
+		return (NULL);
 	alloc = (void *)malloc(count * size);
 	if (alloc == NULL)
 		return (NULL);
